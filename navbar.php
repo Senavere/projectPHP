2f95bb310720d079c309ac7994411b10b0c89266
AnitaMjaow <olsson_anita@live.se>
2022-01-10 20:26:12 +0100

    Update navbar

--- navbar.php
+++ navbar.php
@@ -1,30 +1,17 @@
-<?php
-session_start();
-    $username = $_SESSION['username'];
-    $userid = $_SESSION['user_id'];
-?>
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-
     <link href="css/navbar.css" rel="stylesheet">
 
 </head>
-<header>
     <nav>
-        <img src="img/logo.jpg" alt="logo">
-        <?php echo $username; ?>
-
+        <div class="nav-img">
+        <img src="logo.png" alt="logo">
+        </div>
         <ul>
-            <li><a href=".html">Hem</a></li>
-            <li><a href=".html">...</a></li>
-            <li><a href=".html">...</a></li>
-            <li><a href=".html">...</a></li>
+            <li><a href=".html">🏠</a></li>
+            <li><a href=".html">➡️</a></li>
+            <li><a href=".html">🧭</a></li>
+            <li><a href=".html">💗</a></li>
+            <li><a href=".html"></a></a></li>
 
         </ul>
     </nav>
-</header>
