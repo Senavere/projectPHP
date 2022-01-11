c8ae508922706d37e80a19373ed80fb56e8fc9a2
AnitaMjaow <olsson_anita@live.se>
2022-01-11 05:45:45 +0100

    Ändringar i kod för att hämta från databas

--- php/php_showgallery.php
+++ php/php_showgallery.php
@@ -1,18 +1,24 @@
 <?php
+/* Kollar efter errors*/
+ini_set('display-errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once 'data_connection.php';
-$profileId = $_GET['id']  ?? '';
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
     $pdo = connectToDB();
     $sql = $pdo->prepare('SELECT * FROM posts WHERE user_id = :userid');
-        $sql -> bindValue(':userid', $profileId);
+        $sql -> bindValue(':userid', $_SESSION['user_id']); /* Vi använder $_SESSION för att kalla på aktuella användaren */
         $sql -> execute();
         $posts = $sql -> fetchAll(PDO::FETCH_CLASS);
         foreach($posts as $post){
             echo "<h2>".$post -> title."</h2>";
             echo "<p>".$post -> content."</p>";
-            echo "<img class='profile-img' src='".$post -> picture."' width='300' height='300'>";
+            echo "<img class='profile-img' src='../".$post -> picture."' width='300' height='300'>";
         }
 
 
