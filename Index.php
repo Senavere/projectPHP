c31cdc26e76ee1a9d04a993c3de22917e6bea9b8
AnitaMjaow <olsson_anita@live.se>
2022-01-11 05:29:38 +0100

    styling uppdatering för knapparna  i profilen

--- Index.php
+++ Index.php
@@ -35,7 +35,9 @@ session_start();
 
       <h1 class="profile-user-name"><?php echo $username; ?></h1>
 
-      <button class="btn profile-edit-btn">Edit Profile</button>     <a href="login.php">Login</a><a href="logout.php">Logout</a>
+      <button class="btn profile-btn">Edit Profile</button>
+      <button class="btn profile-btn"><a href="login.php">Login</a></button>
+      <button class="btn profile-btn"><a href="logout.php">Logout</a></button>
 
       <button class="btn profile-settings-btn" aria-label="profile settings"><i class="fas fa-cog" aria-hidden="true"></i></button>
 
